066034598db1626b96281cf6aeb0fc5d035cd575
Christoph Meier <christopmeier@gmail.com>
2018-11-15 20:03:15 +0100

    Add customization for emacs-WIN

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("gnu"   . "http://elpa.gnu.org/packages/"))
 (package-initialize)
 
